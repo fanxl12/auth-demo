62b1d77d210e30dcd23635f27034fe0843c1f517
fanxl12 <744822514@qq.com>
2020-02-14 17:57:16 +0800

    user-server项目集成 Thymeleaf

---
+++ user-server/src/main/java/com/fanxl/auth/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.fanxl.auth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @description
+ * @author: fanxl
+ * @date: 2020/2/14 0014 17:43
+ */
+@RequestMapping("/user")
+@Controller
+public class UserController {
+
+    @GetMapping("")
+    public String all(Model model) {
+        model.addAttribute("name", "张三");
+        return "/user/list";
+    }
+
+}
