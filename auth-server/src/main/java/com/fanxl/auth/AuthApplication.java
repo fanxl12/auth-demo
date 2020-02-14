d89a5ea7022fa7df17cdf9452f42d3e8e6caadfa
fanxl12 <744822514@qq.com>
2020-02-14 17:42:48 +0800

    创建完三个子项目;

---
+++ auth-server/src/main/java/com/fanxl/auth/AuthApplication.java
@@ -0,0 +1,18 @@
+package com.fanxl.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description
+ * @author: fanxl
+ * @date: 2020/2/14 0014 17:40
+ */
+@SpringBootApplication
+public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+
+}
---
+++ user-server/src/main/java/com/fanxl/auth/UserApplication.java
@@ -0,0 +1,18 @@
+package com.fanxl.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description
+ * @author: fanxl
+ * @date: 2020/2/14 0014 17:36
+ */
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}
